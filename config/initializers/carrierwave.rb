1525b276e33a471c69ba96161231a70a2b9100d5
Mac <mac@makkunoMacBook-Pro.local>
2020-12-22 21:37:50 +0900

    修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,16 +4,18 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
     if Rails.env.production?
-        config.storage :fog
-        config.fog_provider = 'fog/aws'
-        config.fog_directory  = 'chokichoki' # 作成したバケット名を記述
-        config.fog_credentials = {
-            provider: 'AWS',
-            aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
-            aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
-            region: 'ap-northeast-1',   # アジアパシフィック(東京)を選択した場合
-            path_style: true
-        }
+        # awsのs3を使う場合
+        # config.storage :fog
+        # config.fog_provider = 'fog/aws'
+        # config.fog_directory  = 'chokichoki' # 作成したバケット名を記述
+        # config.fog_credentials = {
+        #     provider: 'AWS',
+        #     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
+        #     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
+        #     region: 'ap-northeast-1',   # アジアパシフィック(東京)を選択した場合
+        #     path_style: true
+        # }
+        config.storage :file
     else
         config.storage :file
     end
