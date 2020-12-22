1e825814deb50e10bb3fd40b893919009ce7825f
Mac <mac@makkunoMacBook-Pro.local>
2020-12-23 00:34:25 +0900

    しゅうs

--- config/initializers/mail_config.rb
+++ config/initializers/mail_config.rb
@@ -4,7 +4,8 @@ ActionMailer::Base.smtp_settings = {
   domain: 'gmail.com',
   port: 587,
   user_name: 'chokichoki20200401@gmail.com',
-  password: ENV["MAIL_PASSWORD"],
+  #password: ENV["MAIL_PASSWORD"],
+  password: "fwtnfwnmodpejoax",
   authentication: 'plain',
   enable_starttls_auto: true
 }
